0b7c37efd83232e463677983caedf5060888add7
Jonathan Milton <j.milton202@pm.me>
2026-05-10 17:56:12 -0500

    Create subscribe.js

---
+++ netlify/functions/subscribe.js
@@ -0,0 +1,29 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { email } = JSON.parse(event.body);
+
+  const response = await fetch('https://api.beehiiv.com/v2/publications/pub_a10a2657-e4b6-4743-903e-ee2e2661a27a/subscriptions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY_HERE'
+    },
+    body: JSON.stringify({
+      email,
+      reactivate_existing: true,
+      send_welcome_email: true,
+      utm_source: 'agency-rn-calculator',
+      utm_medium: 'website',
+      utm_campaign: 'rn-cost-calculator'
+    })
+  });
+
+  const data = await response.json();
+  return {
+    statusCode: response.status,
+    body: JSON.stringify(data)
+  };
+};
